29e046f3469e1b0b69de77554b306149ae6b92ae
Rohan19258 <109348242+Rohan19258@users.noreply.github.com>
2022-12-03 16:55:21 +0530

    Add files via upload
    DONE

---
+++ src/connection.js
@@ -0,0 +1,12 @@
+const mongoose =require("mongoose")
+
+const DB="mongodb+srv://1234:feeditnot@cluster0.ihvrjfh.mongodb.net/booklist?retryWrites=true&w=majority"
+
+const connection=mongoose.connect(DB,{useNewUrlParser:true,useUnifiedTopology: true}).then(()=>{
+    console.log("connection successful")
+}).catch((e)=>{
+console.log(e.message)
+})
+
+
+module.exports=connection
